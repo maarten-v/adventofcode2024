54f401077714a46b00a94b11a27f584d48f294fe
Maarten <maarten@vliegenthart.org>
2024-12-18 17:15:32 +0100

    3b wip

--- 3b.php
+++ 3b.php
@@ -10,6 +10,17 @@ foreach ($input as $line) {
         preg_match_all('/mul\((\d*),(\d*)\)/', $line, $submatches);
         $total += $submatches[1][$i] * $submatches[2][$i];
     }
+    $begin = strtok($line, 'don\'t');
+    preg_match_all('/mul\((\d*),(\d*)\)/', $begin, $beginMatches);
+    $total += $beginMatches[1][$i] * $beginMatches[2][$i];
+    $lastDo = strrpos($line, 'do');
+    $textAfterLastDo = substr($line, $lastDo + 2, 3);
+    if ($textAfterLastDo !== 'n\'t') {
+        $textAfterLastStart = substr($line, $lastDo + 2);
+        preg_match_all('/mul\((\d*),(\d*)\)/', $begin, $endMatches);
+        var
+        $total += $endMatches[1][$i] * $endMatches[2][$i];
+    }
 }
 
 echo $total;
