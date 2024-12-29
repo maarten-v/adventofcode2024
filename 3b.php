9591a50827890f9ba0961a02be0241b010c2c6e5
Maarten <maarten@vliegenthart.org>
2024-12-29 14:33:58 +0100

    3b wip

--- 3b.php
+++ 3b.php
@@ -2,35 +2,39 @@
 require __DIR__ . '/vendor/autoload.php';
 
 /** @var string[] $input */
-$input = file('input3.txt');
+$input = file('testinput3.txt');
 $total = 0;
 foreach ($input as $line) {
     preg_match_all('/do\(\)\s*(.*?)\s*don\'t\(\)/', $line, $matches);
     $matchCount = count($matches[1]);
-    ray($matches);
     for ($i = 0; $i < $matchCount; $i++) {
-        preg_match_all('/mul\((\d*),(\d*)\)/', $matches[1][$i], $subMatches);
-        $subMatchCount = count($subMatches[0]);
-        for ($j = 0; $j < $subMatchCount; $j++) {
-            echo 'sub ' . $subMatches[1][$j] . ' ' . $subMatches[2][$j] . PHP_EOL;
-            $total += $subMatches[1][$j] * $subMatches[2][$j];
-        }
+        $total += getAmountFromString($matches[1][$i]);
     }
-    $stopPosition = strpos($line, 'don');
+    $firstDoPosition = strpos($line, 'do()');
+    $firstDontPosition = strpos($line, 'don\'t()');
+    $stopPosition = min($firstDontPosition, $firstDoPosition);
     $begin = substr($line, 0, $stopPosition);
-    // add check if there's a do before don't
-    ray($begin);
-    preg_match_all('/mul\((\d*),(\d*)\)/', $begin, $beginMatches);
+    $total += getAmountFromString($begin);
+    $lastDoPosition = strrpos($line, 'do()');
+    $lastDontPosition = strrpos($line, 'don\'t()');
+    if ($lastDontPosition > $lastDoPosition) {
+        continue;
+    }
+    $textAfterLastStart = substr($line, $lastDoPosition + 2);
+    $total += getAmountFromString($textAfterLastStart);
+    echo '----' . PHP_EOL;
+}
 
-    ray($beginMatches);
-    echo 'begin ' . $beginMatches[1][0] . ' ' . $beginMatches[2][0] . PHP_EOL;
-    $total += $beginMatches[1][$i] * $beginMatches[2][$i];
-    $lastDo = strrpos($line, 'do()');
-    $textAfterLastStart = substr($line, $lastDo + 2);
-    preg_match_all('/mul\((\d*),(\d*)\)/', $textAfterLastStart, $endMatches);
-    echo 'end ' . $endMatches[1][0] . ' ' . $endMatches[2][0] . PHP_EOL;
-    $total += $endMatches[1][$i] * $endMatches[2][$i];
-    exit();
+function getAmountFromString(string $matches): int
+{
+    $amount = 0;
+    preg_match_all('/mul\((\d*),(\d*)\)/', $matches, $subMatches);
+    $subMatchCount = count($subMatches[0]);
+    for ($j = 0; $j < $subMatchCount; $j++) {
+        echo $subMatches[1][$j] . ' ' . $subMatches[2][$j] . PHP_EOL;
+        $amount += $subMatches[1][$j] * $subMatches[2][$j];
+    }
+    return $amount;
 }
 
 echo $total;
