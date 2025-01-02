0d19b2d7dc351794141a6413dedd7e9b202dc425
Maarten Vliegenthart <maarten@maximum.nl>
2025-01-02 22:35:34 +0100

    5a

--- 5a.php
+++ 5a.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/vendor/autoload.php';
 
 /** @var string[] $input */
-$input = file('testinput5.txt', FILE_IGNORE_NEW_LINES);
+$input = file('input5.txt', FILE_IGNORE_NEW_LINES);
 $parsingUpdates = false;
 $rules = [];
 $updates = [];
@@ -12,14 +12,39 @@ foreach ($input as $line) {
         continue;
     }
     if ($parsingUpdates) {
-        $updates[] = explode(',',$line);
+        $updates[] = explode(',', $line);
         continue;
     }
     $rules[] = explode('|', $line);
 }
 
-foreach ($updates as $update) {
-    foreach ($rules as $rule) {
-        if (!in_array($rule))
+$total = 0;
+foreach ($updates as $updateIndex => $update) {
+    if (!isUpdateValid($update, $rules)) {
+        continue;
     }
+    $total += $update[(count($update) / 2) - 0.5];
 }
+
+function isUpdateValid(mixed $update, array $rules): bool
+{
+    foreach ($update as $pageIndex => $page) {
+        if ($pageIndex === 0) {
+            continue;
+        }
+        foreach ($rules as $rule) {
+            if ($rule[0] !== $page) {
+                continue;
+            }
+            for ($i = 0; $i < $pageIndex; $i++) {
+                if ($update[$i] === $rule[1]) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+echo $total;
