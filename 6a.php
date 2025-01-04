5451b66f5b6de3e8fa9569136d0c4b192a6304ab
Maarten <maarten@vliegenthart.org>
2025-01-04 20:18:00 +0100

    6a wip

--- 6a.php
+++ 6a.php
@@ -5,8 +5,13 @@ require __DIR__ . '/vendor/autoload.php';
 $input = file('testinput6.txt', FILE_IGNORE_NEW_LINES);
 
 $map = [];
-foreach ($input as $line) {
+foreach ($input as $inputIndex => $line) {
     $map[] = str_split($line);
+    $charPos = strpos($line, '^');
 }
 
-dd($map);
+$outOfBounds = false;
+while (!$outOfBounds) {
+
+    $outOfBounds = true;
+}
