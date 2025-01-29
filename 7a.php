e2d554b6e9538f652158ad03e97a510fa8f2fd97
Maarten <maarten@vliegenthart.org>
2025-01-29 09:00:09 +0100

    7a

--- 7a.php
+++ 7a.php
@@ -3,61 +3,36 @@
 require __DIR__ . '/vendor/autoload.php';
 
 /** @var string[] $input */
-$input = file('testinput7.txt', FILE_IGNORE_NEW_LINES);
+$input = file('input7.txt', FILE_IGNORE_NEW_LINES);
 
 $total = 0;
-//foreach ($input as $line) {
-//    [$resultAsString, $numbersArray] = explode(': ', $line);
-//    $result = (int) $resultAsString;
-//    $numbersAsString = explode(' ', $numbersArray);
-//    $numbers = array_map('intval', $numbersAsString);
-//    if (count($numbers) === 2) {
-//        if ($numbers[0] + $numbers[1] === $result || $numbers[0] * $numbers[1] === $result) {
-//            $total++;
-//            continue;
-//        }
-//    }
-//    $firstNumber = array_shift($numbers);
-//    $operators = ['+', '*'];
-//    foreach ($operators as $operator) {
-//        checkCalculation($result, $firstNumber, $numbers);
-//    }
-//}
-
-if (checkCalculation(190, 0, [1, 2 , 3])) {
-    $total+= 190;
-}
-$counter = 0;
-function checkCalculation($result, $firstNumber, $numbers): bool
-{
-    global $counter;
-    $counter++;
-    if ($counter === 15) {
-        exit();
+foreach ($input as $line) {
+    [$resultAsString, $numbersArray] = explode(': ', $line);
+    $result = (int) $resultAsString;
+    $numbersAsString = explode(' ', $numbersArray);
+    $numbers = array_map('intval', $numbersAsString);
+    if (checkCalculation($result, 0, $numbers)) {
+        $total+= $result;
     }
-    var_dump($firstNumber, $numbers);
+}
+
+function checkCalculation($result, $leftSideOfCalculation, $numbers): bool
+{
     if (count($numbers) === 1) {
-        dump('returning');
-        return $firstNumber + $numbers[0] === $result || $firstNumber * $numbers[0] === $result;
+        return $leftSideOfCalculation + $numbers[0] === $result || $leftSideOfCalculation * $numbers[0] === $result;
     }
     $operators = ['+', '*'];
 
-    //$newFirstNumber = array_shift($numbers);
-
     foreach ($operators as $operator) {
         if ($operator === '+') {
-            $newFirstNumber = $firstNumber + $numbers[0];
-            array_shift($numbers);
-            var_dump($operator);
-            if (checkCalculation($result, $newFirstNumber, $numbers)) {
-                return true;
-            }
+            $newLeftSideOfCalculation = $leftSideOfCalculation + $numbers[0];
         } elseif ($operator === '*') {
-            $newFirstNumber = $firstNumber * $numbers[0];
-            array_shift($numbers);
-            if (checkCalculation($result, $newFirstNumber, $numbers)) {
-                return true;
-            }
+            $newLeftSideOfCalculation = $leftSideOfCalculation * $numbers[0];
+        }
+        $cloneNumbers = $numbers;
+        array_shift($cloneNumbers);
+        if (checkCalculation($result, $newLeftSideOfCalculation, $cloneNumbers)) {
+            return true;
         }
     }
     return false;
